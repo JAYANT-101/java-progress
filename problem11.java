02f7529c71cb83caa68fbebea432cd1c3b06bcb5
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-01-26 00:02:08 +0530

    today

--- problem11.java
+++ problem11.java
@@ -1,5 +1,19 @@
+abstract class voice{
+    abstract void fereqeuncy();
+    abstract void amplitude();
+}
+class human extends voice{
+    public void fereqeuncy(){
+        System.out.println("pitch");
+    }
+    public void amplitude(){
+        System.out.println("loud");
+    }
+}
 public class problem11 {
     public static void main(String[] args) {
-
+human hm=new human();
+hm.fereqeuncy();
+hm.amplitude();
     }
 }
