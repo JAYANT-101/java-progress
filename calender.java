c98386520260de4af399bf854505b2151fcf2ed5
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-05 15:08:33 +0530

    calender

--- calender.java
+++ calender.java
@@ -1,8 +1,15 @@
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class calender {
     public static void main(String[] args) {
         Calendar c=Calendar.getInstance();
         System.out.println(c.getCalendarType());
+        System.out.println(c.get(Calendar.DATE));
+        System.out.println(c.get(Calendar.SECOND));
+        System.out.println(c.get(Calendar.HOUR));
+        System.out.println(c.get(Calendar.HOUR_OF_DAY)+ " : "+ c.get(Calendar.MINUTE));
+        GregorianCalendar cal =new GregorianCalendar();
+        System.out.println(cal.isLeapYear(2020));
     }
 }
