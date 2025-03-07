260c51366519bd8694d65bc43ec3aed92db3cc28
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-07 18:01:54 +0530

    DateTimeFormatter

---
+++ DateTimeFormatterUse.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterUse {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter df2=DateTimeFormatter.ISO_LOCAL_DATE;
+        String myDate=dt.format(df);
+        String myDate2=dt.format(df2);
+        System.out.println(myDate);
+        System.out.println(myDate2);
+    }
+}
