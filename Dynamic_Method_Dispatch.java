fced9e65d568932ba85752030bf0aa30e56589dc
JAYANT-101 <Jayantsinha2005@gmail.com>
2025-03-26 19:37:58 +0530

    new

--- Dynamic_Method_Dispatch.java
+++ Dynamic_Method_Dispatch.java
@@ -15,13 +15,26 @@ class two extends one{
         System.out.println("Aapka swagat hai");
     }
 }
+class three extends two{
+    @Override
+    public void name(){
+        System.out.println("this is three");
+    }
+    public void meth3(){
+        System.out.println("this is meth3");
+    }
+}
 public class Dynamic_Method_Dispatch {
     public static void main(String[] args) {
-//    one obj=new one();
-//    two smboi=new two();
+    one obj=new one();
+    two smboi=new two();
 //    obj.name();
-        one obg=new two();
+//    smboi.greet();
+        two obg=new three();
         obg.greet();
         obg.name();
+        obg.swagat();
+
+
     }
 }
